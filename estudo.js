0f8878414b0c86fb8e4006c9700abd8b63920e28
carolpapaitmaldonado <110677787+carolpapaitmaldonado@users.noreply.github.com>
2022-10-05 08:19:31 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -14,3 +14,21 @@ document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
 document.write(tabuada + " x 10 = "+(tabuada*10)+"<br>");
 document.write("Feito por " + nome);
 }
+
+function minhaTabuada(){
+  for(let i = 1; i <= 10; i++){
+    document.write("Tabuada do " + i + "<br>");
+    for(let j = 1; j <= 10; j++){
+      document.write(i + " x " + j + " = "+(j*i)+"<br>");
+    }
+    document.write("<br>");
+    
+  }
+}
+
+function quadrado (){
+  for(let i = 2; i <= 20; i++){
+    document.write("O quadrado de " + 1 + " é " + (i*i) + "<br>");
+  }
+}
+
