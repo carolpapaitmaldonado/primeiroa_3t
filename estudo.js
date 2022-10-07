9d6ba5684673945a1b653cce897be7d9f63f7c12
carolpapaitmaldonado <110677787+carolpapaitmaldonado@users.noreply.github.com>
2022-10-07 11:07:38 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -32,3 +32,11 @@ function quadrado (){
   }
 }
 
+
+function calcula() {
+  let c = document.getElementById("valor").value;
+  let j = document.getElementById("juros").value;
+  let r = c * (1 + (j/100));
+  document.write("Resultado: " + r);
+}
+
